1e07eb720988b9e78b448ab578b7a055f371b2f9
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-05-04 17:57:49 +0100

    Criaçao de MovingCondition e reorganizaçao de Render

--- 18Ghosts/Render.cs
+++ 18Ghosts/Render.cs
@@ -4,48 +4,48 @@ using System.Text;
 
 namespace _18Ghosts
 {
-        class Render
-        {
-            //Método que demonstra quais os fantasmas que cada jogador tem,        
-            //exemplo:
+    class Render
+    {
+        //Método que demonstra quais os fantasmas que cada jogador tem,        
+        //exemplo:
 
-            /*Fantasmas ativos:
-             *  Player1: R(nº) / G(nº) / B(nº)
-             *  Player2: R(nº) / G(nº) / B(nº)
-             */
-
-            //Método que mostra quais os fantasmas - de cada jogador - que se
-            //encontram nas catacumbas, exemplo:
-
-            /*Catacumbas:
-             *  Player1: R(nº) / G(nº) / B(nº)
-             *  Player2: R(nº) / G(nº) / B(nº)
-             */
-
-            //Método que demonstra de quem é o turno, exemplo:
-
-            /*Turno: P1
+        /*Fantasmas ativos:
+            *  Player1: R(nº) / G(nº) / B(nº)
+            *  Player2: R(nº) / G(nº) / B(nº)
             */
 
-            //Método que apresenta a prioridade dos fantasmas em combate,
-            //exemplo:
+        //Método que mostra quais os fantasmas - de cada jogador - que se
+        //encontram nas catacumbas, exemplo:
 
-            /*R -> G -> B -> R
-            * 
-            * ou:
-            *
-            *    R
-            *  /   \
-            * B --- G
-            * 
+        /*Catacumbas:
+            *  Player1: R(nº) / G(nº) / B(nº)
+            *  Player2: R(nº) / G(nº) / B(nº)
             */
 
-            //UNICODE
-            //poderao dar jeito os seguintes símbolos:
-            // U+142F
-            // U+1431
-            // U+1433
-            // U+1438
+        //Método que demonstra de quem é o turno, exemplo:
 
-        }
+        /*Turno: P1
+        */
+
+        //Método que apresenta a prioridade dos fantasmas em combate,
+        //exemplo:
+
+        /*R -> G -> B -> R
+        * 
+        * ou:
+        *
+        *    R
+        *  /   \
+        * B --- G
+        * 
+        */
+
+        //UNICODE
+        //poderao dar jeito os seguintes símbolos:
+        // U+142F
+        // U+1431
+        // U+1433
+        // U+1438
+
+    }
 }
---
+++ MovingCondition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovingCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

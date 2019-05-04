7e0638008ede4f38b85e56affb2ab5964635f8f7
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-05-04 17:25:56 +0100

    Planeamento do código

--- 18Ghosts/Render.cs
+++ 18Ghosts/Render.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace _18Ghosts
 {
-    class Render
+        class Render
     {
-    }
+            //Método que demonstra quais os fantasmas que cada jogador tem,        
+            //exemplo:
+
+            /*Fantasmas ativos:
+             *  Player1: R(nº) / G(nº) / B(nº)
+             *  Player2: R(nº) / G(nº) / B(nº)
+             */
+
+            //Método que mostra quais os fantasmas - de cada jogador - que se
+            //encontram nas catacumbas, exemplo:
+
+            /*Catacumbas:
+             *  Player1: R(nº) / G(nº) / B(nº)
+             *  Player2: R(nº) / G(nº) / B(nº)
+             */
+
+            //Método que demonstra de quem é o turno, exemplo:
+
+            /*Turno: P1
+            */
+
+            //Método que apresenta a prioridade dos fantasmas em combate, exemplo:
+
+            /*R -> G -> B -> R
+            * 
+            * ou:
+            *
+            *    R
+            *  /   \
+            * B --- G
+            * 
+            */
+
+        }
 }

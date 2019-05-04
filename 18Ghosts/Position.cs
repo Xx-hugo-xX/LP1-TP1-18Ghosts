6485f93e26ac6dd0f16f7a22ef9b6071bfad9a83
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-04 17:22:53 +0100

    Adcionar variáveis e construtor à classe Position

--- 18Ghosts/Position.cs
+++ 18Ghosts/Position.cs
@@ -6,5 +6,13 @@ namespace _18Ghosts
 {
     class Position
     {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }

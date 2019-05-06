c673eb452abd873211c7e5ca3402e290acd0a7e5
Rita Simões Saraiva <belgas2000@gmail.com>
2019-05-07 00:12:20 +0100

    atualizar função CheckforWin

--- 18Ghosts/WinningCondition.cs
+++ 18Ghosts/WinningCondition.cs
@@ -6,29 +6,46 @@ namespace _18Ghosts
 {
     class WinningCondition
     {
-     //se jogo estiver no modo fast tem o jogador tem de eliminar 3 fantasmas
+        //se jogo estiver no modo fast tem o jogador tem de eliminar 3 fantasmas
         //public void modo (string[] args)
-       // {
-           // if jogo == fast //criar fast
-           // {
-           //     Check();
-           // }
+        // {
+        // if jogo == fast //criar fast
+        // {
+        //     Check();
+        // }
         //}
 
         public State Check(Board board)
         {
             if (CheckForWin(ghost, State.P1)) return State.P1;
             if (CheckForWin(ghost, State.P2)) return State.P2;
+
             return State.Undecided; //Ainda niguem ganhou e o estado está 
             //undecided
         }
 
 
-        public bool CheckforWin(State, ghost)
+        public int CheckforWin(Player player)
         {
-            // se no stat ghosts, 3 estiverem no estado out então o 
-            //jogador ganha
-            if (State(ghost, new Out[]
+            Ghosts[] ghostRunAway = new Ghosts[9]; // creates a new array that 
+                                                //saves the ghost who run away
+
+            int runAwayCounter = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                GhostState state = player.ghostList[i].ghostState { get; };
+                
+                //adding to the array the ghosts 
+                if (state == GhostState.Runaway) 
+                {
+                    ghostRunAway[runAwayCounter] = player.ghostList[i];
+                    runAwayCounter++;
+                }
+            }
+
+
+            if (Player(ghost, new Ghosts[]
             {
                 new Out (3)
                 return true;

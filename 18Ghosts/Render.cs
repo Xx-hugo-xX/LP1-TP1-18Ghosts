bf93d1d9f2185c894bce553a4f38b513f64fbe98
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-05-07 10:47:05 +0100

    Ghosts, Players e preparaçao para mapa

--- 18Ghosts/Render.cs
+++ 18Ghosts/Render.cs
@@ -7,28 +7,28 @@ namespace _18Ghosts
     class Render
     {
 
-        // Method that shows the number of active ghosts for both Players
-        //(ghosts in the board)
+        // Method that shows the number of active Ghosts for both Players
+        //(Ghosts in the board)
         private void ActvGhosts(Player p1, Player p2)
         {
 
             Console.WriteLine("Active Ghosts:");
-            //Shows the number of Player1's active ghosts
+            //Shows the number of Player1's active Ghosts
             Console.WriteLine($"Player1: R({}) / Y({}) / B({})");
-            //Shows the number of Player2's active ghosts
+            //Shows the number of Player2's active Ghosts
             Console.WriteLine($"Player2: R({}) / Y({}) / B({})");
 
         }
 
-        // Method that shows the number of both Players captured ghosts (ghosts
+        // Method that shows the number of both Players captured Ghosts (Ghosts
         //in the catacombs)
         private void CapturedGhosts(Player p1, Player p2)
         {
 
             Console.WriteLine("Active Ghosts:");
-            //Shows the number of Player1's captured ghosts
+            //Shows the number of Player1's captured Ghosts
             Console.WriteLine($"Player1: R({}) / Y({}) / B({})");
-            //Shows the number of Player2's captured ghosts
+            //Shows the number of Player2's captured Ghosts
             Console.WriteLine($"Player2: R({}) / Y({}) / B({})");
 
         }
@@ -41,7 +41,7 @@ namespace _18Ghosts
         }
 
         // Method that shows the players which Ghost (color) is superior to
-        //which
+        //which in combat
         private void BattleRules()
         {
             Console.WriteLine("R -> B -> Y -> R");
@@ -53,6 +53,8 @@ namespace _18Ghosts
         // U+1431
         // U+1433
         // U+1438
+
+        // Method that draws the map and Player's Ghosts
 
     }
 }

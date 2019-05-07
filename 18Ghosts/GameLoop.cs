4b4263cd0aa139f2e16908973515f0a8fcfb1392
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-07 01:33:19 +0100

    Estruturar a classe GameLoop com comentários

--- 18Ghosts/GameLoop.cs
+++ 18Ghosts/GameLoop.cs
@@ -6,5 +6,19 @@ namespace _18Ghosts
 {
     class GameLoop
     {
+        //Check if anyone has won the game (while loop)
+
+        //Check who's turn it is
+
+        //Print board on the console
+
+        //Ask what action the player wants to take
+
+        //If he wants to move, run Movement method from Ghost class
+
+        //Else If he wants to save a ghost, run Release method from Ghost class
+
+        //Change turns
+
     }
 }

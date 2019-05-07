5e377bf6966973f3426c8fab79f3d25de85676d0
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-05-07 14:30:42 +0100

    Acrescentar o setter da var. State da Classe Board

--- 18Ghosts/Board.cs
+++ 18Ghosts/Board.cs
@@ -6,18 +6,33 @@ namespace _18Ghosts
 {
     class Board
     {
+        // Instance variables
         private roomState[,] state;
         public roomState currentTurn { get; private set;}
 
+        // Class constructor
         public Board()
         {
             state = new roomState[5, 5];
             currentTurn = state.P1;
         }
 
-        // In this method, its defined wich player's turn it is to play
+        // Setter function for the "state" variable
+        public bool SetState(Position position, roomState newState)
+        {
+            if (newState != currentTurn) return false;
+            if (state[position.Row, position.Column] != roomState.Undecided)
+                return false;
+
+            state[position.Row, position.Column] = newState;
+            ChangeTurn();
+            return true;
+        }
+
+        // Defines which player's turn it is to play.
         private void ChangeTurn()
         {
+            // Changes the turn for the next player.
             if (currentTurn == roomState.P1) currentTurn = roomState.P2;
             else currentTurn = roomState.P1;
         }

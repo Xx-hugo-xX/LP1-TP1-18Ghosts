2e25a3149a9557ff579b501b778f2b8838191390
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-05-07 14:13:13 +0100

    Metodo para troca de turno

--- 18Ghosts/Board.cs
+++ 18Ghosts/Board.cs
@@ -6,5 +6,13 @@ namespace _18Ghosts
 {
     class Board
     {
+        public roomState CurrentTurn { get; private set;}
+
+        // In this method, its defined wich player's turn it is to play
+        private void ChangeTurn()
+        {
+            if (CurrentTurn == roomState.P1) CurrentTurn = roomState.P2;
+            else CurrentTurn = roomState.P1;
+        }
     }
 }

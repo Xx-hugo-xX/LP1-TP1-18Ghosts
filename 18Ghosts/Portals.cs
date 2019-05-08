0461a49d662afcc5dafd4e700ccc98dc82d9ff5f
Rita Simões Saraiva <belgas2000@gmail.com>
2019-05-08 15:15:54 +0100

    Criar classe Portals e PortalState

---
+++ 18Ghosts/Portals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18Ghosts
+{
+    class Portals
+    {
+        // preciso 4 simbolo, mas e imprimido 1 de cada vez
+        // precisa de uma cor
+        // orientação é a casa para q esta a apontar
+        //varios if
+        char symbol;
+        ConsoleColor color;
+        Position orientation;
+
+        //constructer method create a portal
+        public Portals (char symbol, ConsoleColor color, Position position, Position orientation)
+        {
+            this.symbol = symbol;
+            this.color = color;
+            this.position = position;
+            this.orientation = orientation;
+
+        }
+
+        public PortalRotation (Ghost ghost) //rotate the portals door
+        {
+             
+        }
+
+        public KickGhost (Ghost ghost) //send ghost to the out enum
+        {
+
+        }
+    }
+
+}

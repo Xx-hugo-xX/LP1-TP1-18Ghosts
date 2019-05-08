b659ca4128cf58a78dd4a1b63ce4b0c41221d17e
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-08 17:07:44 +0100

    Mudar nome de MovingCondition para Movement

---
+++ 18Ghosts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18Ghosts
+{
+    class Movement
+    {
+
+        public Movement()
+        {
+            //Ask what ghost to move
+
+            //What space to move to
+
+            //MovementCondition(Ghost movingGhost, wantedPosition);
+
+            //If space has a ghost already, call fight method
+
+            //Else if space has a mirror, call mirrorMovement from Mirror class
+
+            //Else, move ghost to wantedPosition
+        }
+    }
+}

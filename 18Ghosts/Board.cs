48dead0807f20fd861ae07b8d5eaa1b566f6191d
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-08 16:58:10 +0100

    Correção da classe Board

--- 18Ghosts/Board.cs
+++ 18Ghosts/Board.cs
@@ -7,24 +7,45 @@ namespace _18Ghosts
     class Board
     {
         // Instance variables
-        private roomState[,] state;
-        public roomState currentTurn { get; private set;}
+        public RoomState[,] state;
+        public RoomType[,] type;
+        public Position[,,] coordinates;
+        public RoomState currentTurn { get; private set;}
 
         // Class constructor
         public Board()
         {
-            state = new roomState[5, 5];
-            currentTurn = state.P1;
+            state = new RoomState[5, 5];
+         
+            
+            /*
+            type = new RoomType[5, 5] {{RoomType.Blue, RoomType.Red,
+                    RoomType.Portal, RoomType.Blue, RoomType.Red},
+                    { RoomType.Yellow, RoomType.Mirror, RoomType.Yellow,
+                    RoomType.Mirror, RoomType.Yellow},
+                    { RoomType.Red, RoomType.Blue, RoomType.Red,
+                    RoomType.Blue, RoomType.Portal},
+                    { RoomType.Blue, RoomType.Mirror, RoomType.Yellow,
+                    RoomType.Mirror, RoomType.Red},
+                    { RoomType.Yellow, RoomType.Red, RoomType.Portal,
+                    RoomType.Blue, RoomType.Yellow}}; 
+                */
+
+
+            coordinates = new Position[5, 5, 4];
+            currentTurn = RoomState.P1;
         }
 
+
+
         // Getter function for the "state" variable
-        public roomState GetState(Position position)
+        public RoomState GetState(Position position)
         {
             return state[position.Row, position.Column];
         }
 
         // Setter function for the "state" variable
-        public bool SetState(Position position, roomState newState)
+        public bool SetState(Position position, RoomState newState)
         {
             // Defines new value for desired position and calls ChangeTurn()
             state[position.Row, position.Column] = newState;
@@ -33,11 +54,21 @@ namespace _18Ghosts
         }
 
         // Defines which player's turn it is to play.
-        private void ChangeTurn()
+        public void ChangeTurn()
         {
             // Changes the turn for the next player.
-            if (currentTurn == roomState.P1) currentTurn = roomState.P2;
-            else currentTurn = roomState.P1;
+            if (currentTurn == RoomState.P1) currentTurn = RoomState.P2;
+            else currentTurn = RoomState.P1;
         }
+
+        /*public Position GetCoordinate(Position playerPosition)
+        {
+
+
+        }
+        */
+       
+
+
     }
 }

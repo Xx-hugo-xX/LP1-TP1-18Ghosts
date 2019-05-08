152b86220c8ad7124f31dcec662d73fea4dc287f
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-09 00:50:38 +0100

    Ciclos para obter lista de fantasmas na masmorra

--- 18Ghosts/GhostRespawn.cs
+++ 18Ghosts/GhostRespawn.cs
@@ -6,5 +6,28 @@ namespace _18Ghosts
 {
     class GhostRespawn
     {
+        public void Respawn(Player currentPlayer, Player adversary)
+        {
+            int dungeonCounter = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (currentPlayer.ghostList[i].ghostState == GhostState.Locked)
+                    dungeonCounter++;
+            }
+
+            Ghost[] dungeonGhosts = new Ghost[dungeonCounter];
+            int adding = 0;
+
+            for (int i = 0; i < dungeonCounter; i++)
+            {
+                if (currentPlayer.ghostList[i].ghostState == GhostState.Locked)
+                {
+                    dungeonGhosts[adding] = currentPlayer.ghostList[i];
+                    adding++;
+                }
+            }
+
+        }
     }
 }

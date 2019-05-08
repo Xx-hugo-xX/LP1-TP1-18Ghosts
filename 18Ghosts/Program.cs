3f365658858e5e2774c00e242e8e1e00ee4ddec3
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-08 17:10:25 +0100

    Criar instancias das classes dentro do Program

--- 18Ghosts/Program.cs
+++ 18Ghosts/Program.cs
@@ -6,7 +6,23 @@ namespace _18Ghosts
     {
         static void Main(string[] args)
         {
-            
+            Board board = new Board();
+
+            CheckforWin winningCondition = new CheckforWin();
+
+            Render render = new Render();
+
+            Player p1 = new Player();
+            Player p2 = new Player();
+
+            render.DrawMap();
+
+            string nome = p1.R1.name; 
+
+            Console.WriteLine($"{nome}");
+
+
+                
         }
     }
 }

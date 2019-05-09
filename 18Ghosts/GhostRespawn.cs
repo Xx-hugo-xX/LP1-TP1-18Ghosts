0408a7f8cc93233d5dc8d5701de360b83a495da2
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-09 02:53:14 +0100

    Terminar a classe GhostRespawn

--- 18Ghosts/GhostRespawn.cs
+++ 18Ghosts/GhostRespawn.cs
@@ -54,7 +54,11 @@ namespace _18Ghosts
             board.ChangeTurn();
 
             Console.WriteLine("Where does the adversary want to" +
-                "put the ghost? (1-25)");
+                "put the ghost?");
+            Console.WriteLine(" Space has to be free and the same" +
+                "color as the ghost.");
+            Console.WriteLine("Can't be a mirror or a portal (1-25)");
+
 
             bool validSpace = false;
             option = 0;
@@ -67,10 +71,52 @@ namespace _18Ghosts
                 if (option < 1 || option > 25)
                 {
                     wantedPosition = currentPlayer.PositionForNumber(option);
-                    if (true)
+                    if (savedGhost.color == ConsoleColor.Red &&
+                        board.type[wantedPosition.Row, wantedPosition.Column]
+                        == RoomType.Red && board.state[wantedPosition.Row,
+                        wantedPosition.Column] == RoomState.Undecided)
                     {
-
+                        MakeChanges(currentPlayer, savedGhost, wantedPosition);
+                        validSpace = true;
                     }
+                    else if (savedGhost.color == ConsoleColor.Blue &&
+                        board.type[wantedPosition.Row, wantedPosition.Column]
+                        == RoomType.Blue && board.state[wantedPosition.Row,
+                        wantedPosition.Column] == RoomState.Undecided)
+                    {
+                        MakeChanges(currentPlayer, savedGhost, wantedPosition);
+                        validSpace = true;
+                    }
+                    else if (savedGhost.color == ConsoleColor.Yellow &&
+                        board.type[wantedPosition.Row, wantedPosition.Column]
+                        == RoomType.Yellow && board.state[wantedPosition.Row,
+                        wantedPosition.Column] == RoomState.Undecided)
+                    {
+                        MakeChanges(currentPlayer, savedGhost, wantedPosition);
+                        validSpace = true;
+                    }
+                    else Console.WriteLine("That position is unavailable.");
+                }
+                else Console.WriteLine("That option is not valid.");
+            }
+            board.ChangeTurn();
+        }
+
+
+        public void MakeChanges(Player currentPlayer, Ghost savedGhost,
+            Position wantedPosition)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (currentPlayer.ghostList[i].name == savedGhost.name)
+                {
+                    currentPlayer.ghostList[i].ghostState = GhostState.Inside;
+
+                    currentPlayer.ghostList[i].position.Row
+                        = wantedPosition.Row;
+
+                    currentPlayer.ghostList[i].position.Column
+                        = wantedPosition.Column;
                 }
             }
         }
